58d998c6d3f9043bc32e10abc56b9bb4ac1e2ca5
Luis Cesar <lutzcc1@gmail.com>
2020-07-11 14:19:50 -0500

    update controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,17 +4,15 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class MainController extends AbstractController
 {
     /**
-     * @Route("/main", name="main")
+     * @Route("/", name="home")
      */
     public function index()
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MainController.php',
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
