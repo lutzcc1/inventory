0b7452c178028be616d17e739ab51c6bdb52ac9e
Luis Cesar <lutzcc1@gmail.com>
2020-07-13 09:16:58 -0500

    corrected routes

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -7,7 +7,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
-use App\Repository\ItemRepository;
 use App\Form\ItemFormType;
 
 /**
@@ -16,27 +15,7 @@ use App\Form\ItemFormType;
 class ItemController extends AbstractController
 {
     /**
-     * @Route("", name="index")
-     */
-    public function index(ItemRepository $itemRepository)
-    {
-        $item = new Item();
-        $items = $itemRepository->findAll();
-        $form = $this->createForm(ItemFormType::class, $item);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-          dump($item);
-        }
-
-        return $this->render('item/index.html.twig', [
-            'items' => $items,
-            'form' => $form->createView()
-        ]);
-    }
-
-    /**
      * @Route("/create", name="create")
-     * @return Response
      */
     public function create(Request $request) {
         $item = new Item();
@@ -48,7 +27,7 @@ class ItemController extends AbstractController
         $em->persist($item);
         $em->flush();
 
-        return $this->redirect($this->generateUrl('item.index'));
+        return $this->redirect($this->generateUrl('home'));
     }
 
     /**
@@ -59,6 +38,6 @@ class ItemController extends AbstractController
       $em->remove($item);
       $em->flush();
 
-      return $this->redirect($this->generateUrl('item.index'));
+      return $this->redirect($this->generateUrl('home'));
     }
 }
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,14 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Item;
+use App\Repository\ItemRepository;
+use App\Form\ItemFormType;
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(ItemRepository $itemRepository)
     {
-        return $this->render('home/index.html.twig');
+        $item = new Item();
+        $items = $itemRepository->findAll();
+        $form = $this->createForm(ItemFormType::class, $item);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+          dump($item);
+        }
+
+        return $this->render('item/index.html.twig', [
+            'items' => $items,
+            'form' => $form->createView()
+        ]);
     }
 }
