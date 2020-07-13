261e9dfdad7bb6dfb39cf12f8e835e59ba44ddc7
Luis Cesar <lutzcc1@gmail.com>
2020-07-12 20:43:13 -0500

    price is now a float instead of integer

--- src/Entity/Item.php
+++ src/Entity/Item.php
@@ -28,7 +28,7 @@ class Item
     private $quantity;
 
     /**
-     * @ORM\Column(type="integer")
+     * @ORM\Column(type="float")
      */
     private $price;
 
@@ -66,12 +66,12 @@ class Item
         return $this;
     }
 
-    public function getPrice(): ?int
+    public function getPrice(): ?float
     {
         return $this->price;
     }
 
-    public function setPrice(int $price): self
+    public function setPrice(float $price): self
     {
         $this->price = $price;
 
